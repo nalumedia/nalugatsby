cab45c4a0b5fc5902bf44d0dceed8859beed52f9
nalumedia <ryan.dranginis@gmail.com>
2021-02-23 06:10:06 -0800

    added pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
+import Header from "./components/header"
 import { Link } from "gatsby"
 
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact Yo</Link>
-      <p>What a world.</p>
+      <Link to="/contact/">Contact</Link> &nbsp; 
+      <Link to="/about/">About</Link>
+      <Header headerText="Homepage" />
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
